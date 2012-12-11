6ea7af9895b7018e4b16db212e0e46c472f6ad41
lycantrophe <lycantrophe@lavabit.com>
2012-12-11 22:43:11 +0100

    Added vector size reduction when popping.

--- stack.c
+++ stack.c
@@ -2,5 +2,13 @@
 #include "vector.h"
 
 void* pop( Vector* V ) {
+    /* Frees some memory if less than 2/3
+     * of the allocated space is being used 
+     */
+    if( V->capacity - ( V->size * V->item_size ) > (float)2*V->capacity / 3 ) {
+        V->capacity /= 2;
+        V->base = realloc( V->base, V->capacity );
+    }
+
     return (char*)V->base + ( --( V->size ) * V->item_size );
 }

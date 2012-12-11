0e2e009adb8f48f3e678d64e981eff5aab51404c
lycantrophe <lycantrophe@lavabit.com>
2012-12-11 20:24:08 +0100

    Added some comments to header file

--- vector.h
+++ vector.h
@@ -11,8 +11,21 @@ typedef struct {
     size_t item_size;
 } Vector;
 
+/*
+ * Creates a new vector.
+ */
 extern Vector* vector( size_t typesize, size_t icap );
+
+/*
+ * Insert an element at the back of the vector.
+ */
 extern void push( Vector*, void* );
+
+/*
+ * Binary search the vector, looking for the target element. Takes a function
+ * pointer to a comparison function that should return positive if the first
+ * argument is less than the second argument.
+ */
 extern void* find( Vector*, cmp lt, void* target );
 
 #endif //VECTOR_H

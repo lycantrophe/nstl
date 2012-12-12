73193650f004a90bfa016360ff048bdc32faa4c4
Yggdrasill <yggdrasill@lavabit.com>
2012-12-12 19:32:11 +0100

    inb4 lyc criticism

--- queue.c
+++ queue.c
@@ -1,12 +1,17 @@
 #include "queue.h"
 
 void enqueue( Queue* Q, void* item ) {
-    unsigned long first = ( Q->V.base - Q->first ) / Q->V.item_size;
+    unsigned long first = ( Q->first - Q->V.base ) / Q->V.item_size;
+    unsigned long thresh = 2 * ( Q->V.capacity / Q->V.item_size ) / 3;
 
-    if( first >= 2 * ( Q->V.capacity / Q->V.item_size ) / 3 )
+    if( Q->V.size - first <= thresh && first >= thresh ) {
         memmove( Q->V.base, Q->first, first - ( unsigned long ) Q->V.base );
-    if( Q->V.capacity - ( Q->V.size * Q->V.item_size ) > 2 * Q->V.capacity / 3)
-        Q->V.base = realloc(Q->V.base, Q->V.capacity /= 2 );
+        Q->V.size -= first;
+        Q->first = Q->V.base;
+
+        if( Q->V.size <= thresh )
+            Q->V.base = realloc( Q->V.base, Q->V.capacity /= 2 );
+    }
 
     push( &Q->V, item );
 }

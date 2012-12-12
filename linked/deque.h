f97ecda213b12c58030af780405fffce33d7ee5b
lycantrophe <lycantrophe@lavabit.com>
2012-12-12 22:59:11 +0100

    Added comments to header files.

--- linked/deque.h
+++ linked/deque.h
@@ -3,14 +3,30 @@
 
 #include "list.h"
 
+/*
+ * O(1) linked list dequeue implementation.
+ */
+
 typedef struct {
     unsigned int size;
     List* head;
     List* tail;
 } Deque;
 
+/*
+ * Constructs a default Deque object.
+ */
 extern Deque* deque( void );
+
+/*
+ * Enqueues a node with the given payload.
+ */
 extern void deenqueue( Deque*, void* );
+
+/*
+ * Shifts the first item from the dequeue and returns its payload. Reduces size
+ * by one. Will fail if queue is empty.
+ */
 extern void* dedequeue( Deque* );
 
 #endif //DEQUE_H
--- linked/list.h
+++ linked/list.h
@@ -7,7 +7,16 @@ typedef struct List {
     struct List* prev;
 } List;
 
-extern List* link( List*, void* );
+/*
+ * Creates a new node with the passed payload and appends it onto the given
+ * list. If list is NULL both next and prev in the returned node is set to
+ * NULL.
+ */
+extern List* link( List* list, void* payload );
+
+/*
+ * Unlinks the given List item from its list. Binds its next/prev together
+ */
 extern void* unlink( List* );
 
 

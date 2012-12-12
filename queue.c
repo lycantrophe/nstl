92632177c7cd527be67a0f16c2fc865e157ce02a
Yggdrasill <yggdrasill@lavabit.com>
2012-12-12 18:17:46 +0100

    Syntax-fixes, added checking for unused space on right side of queue

--- queue.c
+++ queue.c
@@ -1,10 +1,12 @@
 #include "queue.h"
 
 void enqueue( Queue* Q, void* item ) {
-    unsigned int first = (Q->V.base - Q->first) / Q->V.item_size;
-    if(first >= 2 * (Q->V.capacity / Q->V.item_size) / 3) {
-        memmove(Q->V.base, Q->first, Q->V.base - first);
-        Q->V.base = realloc(Q->V.base, Q->V.capacity);
-    }
+    unsigned long first = ( Q->V.base - Q->first ) / Q->V.item_size;
+
+    if( first >= 2 * ( Q->V.capacity / Q->V.item_size ) / 3 )
+        memmove( Q->V.base, Q->first, first - ( unsigned long ) Q->V.base );
+    if( Q->V.capacity - ( Q->V.size * Q->V.item_size ) > 2 * Q->V.capacity / 3)
+        Q->V.base = realloc(Q->V.base, Q->V.capacity /= 2 );
+
     push( &Q->V, item );
 }
--- queue.h
+++ queue.h
@@ -1,6 +1,9 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <string.h>
+#include "vector.h"
+
 typedef struct {
     Vector V;
     void* first;

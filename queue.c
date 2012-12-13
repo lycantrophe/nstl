69492fb5d254774d7b2f647b7a8ed1ab8197b283
Yggdrasill <yggdrasill@lavabit.com>
2012-12-13 08:11:59 +0100

    Improved move and realloc detection. Hopefully this pleases the lyc

--- queue.c
+++ queue.c
@@ -1,16 +1,30 @@
 #include "queue.h"
 
+static move( Queue* Q, unsigned long first ) {
+    memmove( Q->V.base, Q->first, first - ( unsigned long ) Q->V.base );
+    Q->V.size -= first;
+    Q->first = Q->V.base;
+}
+
 void enqueue( Queue* Q, void* item ) {
     unsigned long first = ( Q->first - Q->V.base ) / Q->V.item_size;
     unsigned long thresh = 2 * ( Q->V.capacity / Q->V.item_size ) / 3;
+    unsigned long lthresh = ( Q->V.capacity / Q->V.item_size ) - thresh;
 
-    if( Q->V.size - first <= thresh && first >= thresh ) {
-        memmove( Q->V.base, Q->first, first - ( unsigned long ) Q->V.base );
-        Q->V.size -= first;
-        Q->first = Q->V.base;
-
-        if( Q->V.size <= thresh )
-            Q->V.base = realloc( Q->V.base, Q->V.capacity /= 2 );
+    if( Q->V.size == Q->V.capacity / Q->V.item_size && first >= thresh ) {
+        move( Q, first );
+        first = ( Q->first - Q->V.base ) / Q->V.item_size;
+    }
+    if( Q->V.size - first <= thresh / 2) {
+        if( first >= thresh ) {
+            move( Q, first );
+            first = ( Q->first - Q->V.base ) / Q->V.item_size;
+        }
+        else
+          Q->V.base = realloc( Q->V.base, Q->V.capacity /= 2);
+    }
+    if( first >= lthresh ) {
+        move( Q, first );
     }
 
     push( &Q->V, item );

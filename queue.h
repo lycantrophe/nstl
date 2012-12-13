fa994321f4caae2c6cd9d44d48b9cea7034f433c
lycantrophe <lycantrophe@lavabit.com>
2012-12-13 21:11:50 +0100

    Written Queue vector extension.
    Provides a vector-based FIFO queue that extracts from left side and pushes onto
    the right side.

---
+++ queue.h
@@ -0,0 +1,20 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <string.h>
+#include "vector.h"
+
+typedef struct {
+    Vector V;
+    void* first;
+    unsigned int items;
+} Queue;
+
+/*
+ * Creates a new queue
+ */
+extern Queue* queue( size_t typesize, unsigned int icap );
+extern void enqueue( Queue*, void* );
+extern void* dequeue( Queue* );
+
+#endif //QUEUE_H

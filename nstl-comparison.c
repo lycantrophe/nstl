9329f8278855188673d9e30d411a2b18c7eeb24e
lycantrophe <lycantrophe@lavabit.com>
2012-12-31 13:31:47 +0100

    Fixed mis-implementation of nstl comparison functions.

--- nstl-comparison.c
+++ nstl-comparison.c
@@ -4,30 +4,26 @@ unsigned short int_lt( void* lhs, void* rhs ) {
     return *(int*)lhs < *(int*)rhs;
 }
 
-unsigned short int_lt( void* lhs, void* rhs ) {
+unsigned short uint_lt( void* lhs, void* rhs ) {
     return *(unsigned int*)lhs < *(unsigned int*)rhs;
 }
 
-unsigned short int_lt( void* lhs, void* rhs ) {
+unsigned short long_lt( void* lhs, void* rhs ) {
     return *(long*)lhs < *(long*)rhs;
 }
 
-unsigned short int_lt( void* lhs, void* rhs ) {
+unsigned short longlong_lt( void* lhs, void* rhs ) {
     return *(long long*)lhs < *(long long*)rhs;
 }
 
-unsigned short int_lt( void* lhs, void* rhs ) {
+unsigned short char_lt( void* lhs, void* rhs ) {
     return *(char*)lhs < *(char*)rhs;
 }
 
-unsigned short int_lt( void* lhs, void* rhs ) {
+unsigned short double_lt( void* lhs, void* rhs ) {
     return *(double*)lhs < *(double*)rhs;
 }
 
-unsigned short int_lt( void* lhs, void* rhs ) {
-    return *(unsigned double*)lhs < *(unsigned double*)rhs;
-}
-
-unsigned short int_lt( void* lhs, void* rhs ) {
+unsigned short float_lt( void* lhs, void* rhs ) {
     return *(float*)lhs < *(float*)rhs;
 }
